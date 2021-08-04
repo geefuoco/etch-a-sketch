e21be108fa0e844e3d5d2b2dcec56ffcc47a6f8a
geefuoco <gianluca_1227@hotmail.com>
2021-08-04 13:22:07 -0400

    Added some CSS and JS. Events and drawing functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,33 @@
 const LENGTH = 16;
 const WIDTH = 16;
 const content = document.querySelector("#content");
+const clear = document.querySelector("#clear-button");
 
 function createEtchBlocks(){
     for(let x=1; x <=LENGTH;x++){
         for(let y=1; y<=WIDTH;y++){
             let div = document.createElement("div");
+
             div.classList.add("etch-cell");
-            div.style.gridArea = " x/y/x+1/y+1";
-            
+            div.style.gridArea = `${x}/${y}/${x+1}/${y+1}`;
+            div.style.backgroundColor = "white";
+
+            div.addEventListener("mouseenter", (e)=>{
+                e.target.style.backgroundColor = "black";
+            });
+
+            content.appendChild(div);
         }
     }
 }
+
+clear.addEventListener("click", (e)=>{
+    let cells = document.querySelectorAll(".etch-cell");
+    cells = Array.from(cells);
+    cells.forEach(element =>{
+        element.style.backgroundColor= "white";
+    });
+});
+
+
+createEtchBlocks();
